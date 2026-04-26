335bd7502cbc31305e767bfaa1d3d2e422ecb331
dotlake <dotlake@CHATTY-STICK>
2026-04-25 21:06:25 -0600

    more fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# Version 0.1.4 (04-25-2026)
+
+Fixed again, whitelist was broken
+
 # Version 0.1.3 (04-24-2026)
 
 Fix item saving
--- Patches/AddStash.cs
+++ Patches/AddStash.cs
@@ -1,10 +1,10 @@
-﻿using HarmonyLib;
+﻿using BepInEx.Logging;
+using HarmonyLib;
 using System.Linq;
-using UnityEngine.UIElements;
 
 namespace IronStash.Patches
 {
-    [HarmonyPatch(typeof(CL_GameManager), "Start")]
+    [HarmonyPatch(typeof(CL_GameManager), "LoadIn")]
     public class AddStash
     {
         [HarmonyPostfix]
@@ -17,13 +17,13 @@ namespace IronStash.Patches
                 for (int i = 0; i < SettingsManager.settings.competitiveSettings.lockerItems.Count; i++)
                 {
                     Item_Object itemObjectPrefab = CL_AssetManager.GetItemObjectPrefab(SettingsManager.settings.competitiveSettings.lockerItems[i]);
-                    if (itemObjectPrefab != null && whitelist.Contains(itemObjectPrefab.itemData.itemName))
+                    if (itemObjectPrefab != null && whitelist.Contains(itemObjectPrefab.name.ToLower()))
                     {
                         string data = SettingsManager.settings.competitiveSettings.lockerItems[i];
                         CL_GameManager.SetGameFlag("equiplocker-"
                             + CL_GameManager.GetBaseGamemode().GetGamemodeName()
                             + "local"
-                            + (i + 1), state: true, data, save: true);
+                            + (i + 1), state: true, data);
                     }
                 }
             }
